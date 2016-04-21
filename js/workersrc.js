31d4e53fe6e47353c1e7ba15ed07df29e1c174c9
Lukas Reschke <lukas@owncloud.com>
2016-04-21 16:50:39 +0200

    [stable8.1] Redirect page if not shown in an iframe

--- js/workersrc.js
+++ js/workersrc.js
@@ -1,1 +1,15 @@
+/**
+ * Checks if the page is displayed in an iframe. If not redirect to /.
+ **/
+function redirectIfNotDisplayedInFrame () {
+	try {
+		if (window.frameElement) {
+			return;
+		}
+	} catch (e) {}
+
+	window.location.href = '/';
+}
+redirectIfNotDisplayedInFrame();
+
 PDFJS.workerSrc = document.getElementsByTagName('head')[0].getAttribute('data-workersrc');
