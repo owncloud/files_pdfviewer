16d50b0a5a926d6af8d4e1fc9338e3949c712b1b
Jan Ackermann <jackermann@owncloud.com>
2021-04-15 00:12:43 +0000

    [tx] updated from transifex

--- l10n/pt_BR.js
+++ l10n/pt_BR.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "files_pdfviewer",
     {
+    "This shared file does not have download permission and is possibly protected by secure view, please contact the owner of the file for granting permission or use a different viewer." : "Este arquivo compartilhado não tem permissão para ser baixado e possivelmente está protegido por visualização segura. Entre em contato com o proprietário do arquivo para conceder permissão ou use um visualizador diferente. ",
     "Open in PDF Viewer" : "Abrir no visualizador de PDF",
     "Close" : "Fechar"
 },
