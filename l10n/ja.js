57bd9d47117ce2d8dbf32dfb78f52e7d87e2c05b
ownClouders <devops@owncloud.com>
2022-05-14 00:06:45 +0000

    [tx] updated from transifex

---
+++ l10n/ja.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "files_pdfviewer",
+    {
+    "This shared file does not have download permission and is possibly protected by secure view, please contact the owner of the file for granting permission or use a different viewer." : "この共有ファイルはダウンロード権限がなく、セキュアビューで保護されている可能性があります。ファイルの所有者に連絡して、権限を付与するか、別のビューアを使用してください。",
+    "Open in PDF Viewer" : "PDFビューアで開く",
+    "Close" : "閉じる"
+},
+"nplurals=1; plural=0;");
