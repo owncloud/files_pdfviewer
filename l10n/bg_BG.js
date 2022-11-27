2a2162b1ff81c3a239b5732707300ec4a2854469
ownClouders <devops@owncloud.com>
2022-11-27 01:14:04 +0000

    [tx] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "files_pdfviewer",
     {
-    "This shared file does not have download permission and is possibly protected by secure view, please contact the owner of the file for granting permission or use a different viewer." : "За този споделен файл нямате необходимите права за изтегляне и вероятно е защитен със защитен преглед, моля свържете се със собственика на файла за да ви предостави тези права, или използвайте друг преглед.",
+    "This shared file does not have download permission and is possibly protected by secure view, please contact the owner of the file for granting permission or use a different viewer." : "За този споделен файл нямате необходимите права за изтегляне и вероятно е защитен със защитен преглед, моля свържете се със собственика на файла, за да ви предостави тези права, или използвайте друг преглед.",
     "Open in PDF Viewer" : "Отваряне в PDF преглед",
     "Close" : "Затваряне"
 },
