7359feb563cd50796d0e082db89c4738409722c5
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2024-01-11 12:37:16 +0100

    feat: config switch to enable scripting

--- js/workersrc.js
+++ js/workersrc.js
@@ -23,15 +23,16 @@ function deferredViewerConfig() {
 		);
 	}
 	try {
+		var head = document.getElementsByTagName('head')[0]
 		PDFViewerApplicationOptions.set('disablePreferences', true);
-		PDFViewerApplicationOptions.set('workerSrc', document.getElementsByTagName('head')[0].getAttribute('data-workersrc'));
+		PDFViewerApplicationOptions.set('workerSrc', head.getAttribute('data-workersrc'));
 		PDFViewerApplicationOptions.set('locale', getSanitizedCurrentLocale());
-		PDFViewerApplicationOptions.set('cMapUrl', document.getElementsByTagName('head')[0].getAttribute('data-cmapurl'));
-		PDFViewerApplicationOptions.set('sandboxBundleSrc', document.getElementsByTagName('head')[0].getAttribute('data-sandbox'));
+		PDFViewerApplicationOptions.set('cMapUrl', head.getAttribute('data-cmapurl'));
+		PDFViewerApplicationOptions.set('sandboxBundleSrc', head.getAttribute('data-sandbox'));
 		PDFViewerApplicationOptions.set('printResolution', 300);
 		PDFViewerApplicationOptions.set('externalLinkTarget', pdfjsLib.LinkTarget.BLANK);
 		PDFViewerApplicationOptions.set('isEvalSupported', false);
-		PDFViewerApplicationOptions.set('enableScripting', false);
+		PDFViewerApplicationOptions.set('enableScripting', head.getAttribute('data-enableScripting') === 'true');
 	} catch (e) {
 	}
 }
