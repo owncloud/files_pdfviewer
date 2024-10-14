299f9b97ff751bf05676801f48bbb9baacf9352e
ownClouders <devops@owncloud.com>
2024-10-14 01:38:01 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "files_pdfviewer",
+    {
+    "This shared file does not have download permission and is possibly protected by secure view, please contact the owner of the file for granting permission or use a different viewer." : "بۇ ھەمبەھىرلەنگەن ھۆججەتنىڭ چۈشۈرۈش ئىجازەتنامىسى يوق ھەمدە بىخەتەر كۆرۈنۈش تەرىپىدىن قوغدىلىشى مۇمكىن ، ئىجازەتنامە ئېلىش ياكى باشقا كۆرگۈچنى ئىشلىتىش ئۈچۈن ھۆججەت ئىگىسى بىلەن ئالاقىلىشىڭ.",
+    "Open in PDF Viewer" : "PDF Viewer دا ئېچىڭ",
+    "Close" : "ياپ"
+},
+"nplurals=2; plural=(n != 1);");
